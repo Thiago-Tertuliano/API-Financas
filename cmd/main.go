8374523ff95cf312f13c7f03a874384f5c41bb95
Thiago Tertuliano <thiagomatostertuliano@gmail.com>
2025-08-14 17:51:48 -0300

    Main.go # Commit para adicionar o comando de rotas e integração swagger

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/thiago-tertuliano/Financa-API/docs"
+	// _ "github.com/thiago-tertuliano/Financa-API/docs"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	swaggerFiles "github.com/swaggo/files"
 	"log"
@@ -11,3 +11,35 @@ import (
 
 // @title Financa API
 // @version 1.0
+// @descriptions API de Controle Financeiro em GO
+// @host localhost:8080
+// @BasePath /api/v1
+
+func main() {
+	r := gin.Default()
+
+	// Rotas públicas
+	v1 := r.Group("/api/v1")
+	{
+		v1.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{"message": "pong"})
+		})
+
+		// Rotas de autenticação
+	}
+
+	// Rotas Protegidas (exemplo, depois adicionar middleware de autenticação)
+	// auth := v1.Group("/")
+	// auth.Use(AuthMiddleware())
+	// {
+	//     auth.GET("/transactions", ListTransactions)
+	//     auth.POST("/transactions", CreateTransaction)
+	//	   // etc.
+	//}
+
+	// Swagger docs
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	log.Println("Servidor rodando na porta 8080")
+    r.Run(":8080")
+}
