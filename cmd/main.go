debdf51044ea1592db7fa312a6f63442765fa429
Thiago Tertuliano <thiagomatostertuliano@gmail.com>
2025-08-14 17:41:57 -0300

    API de Finanças

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thiago-tertuliano/Financa-API/docs"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+	"log"
+
+)
+
+// @title Financa API
+// @version 1.0
